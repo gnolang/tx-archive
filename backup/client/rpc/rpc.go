4c2204bfbcf3f9daa97f7156e93a3120581ae911
aeddi <antoine.e.b@gmail.com>
2024-09-05 15:12:03 +0200

    feat(backup): add batch feature to rpc client

--- backup/client/rpc/rpc.go
+++ backup/client/rpc/rpc.go
@@ -2,11 +2,14 @@ package rpc
 
 //nolint:revive // See https://github.com/gnolang/gno/issues/1197
 import (
+	"context"
+	"errors"
 	"fmt"
 
 	_ "github.com/gnolang/gno/gno.land/pkg/sdk/vm"
 	"github.com/gnolang/gno/tm2/pkg/amino"
 	rpcClient "github.com/gnolang/gno/tm2/pkg/bft/rpc/client"
+	ctypes "github.com/gnolang/gno/tm2/pkg/bft/rpc/core/types"
 	"github.com/gnolang/gno/tm2/pkg/std"
 
 	"github.com/gnolang/tx-archive/backup/client"
@@ -55,37 +58,75 @@ func (c *Client) GetLatestBlockNumber() (uint64, error) {
 	return uint64(status.SyncInfo.LatestBlockHeight), nil
 }
 
-func (c *Client) GetBlock(blockNum uint64) (*client.Block, error) {
-	// Fetch the block
-	blockNumInt64 := int64(blockNum)
+func (c *Client) GetBlocks(ctx context.Context, from, to uint64) ([]*client.Block, error) {
+	// Check if the block range is valid
+	if from > to {
+		return nil, fmt.Errorf(
+			"invalid block range, from (%d) bigger than to (%d)",
+			from,
+			to,
+		)
+	}
 
-	block, err := c.client.Block(&blockNumInt64)
+	// Prepare batch of requests
+	batch := c.client.NewBatch()
+
+	for currBlock := from; currBlock <= to; currBlock++ {
+		// Add current block to the batch
+		currBlockInt64 := int64(currBlock)
+
+		if err := batch.Block(&currBlockInt64); err != nil {
+			return nil, fmt.Errorf(
+				"unable to batch block request, %w",
+				err,
+			)
+		}
+	}
+
+	// Send batch of requests
+	results, err := batch.Send(ctx)
 	if err != nil {
 		return nil, fmt.Errorf(
-			"unable to fetch block, %w",
+			"unable to send batch of request, %w",
 			err,
 		)
 	}
 
-	// Decode amino transactions
-	txs := make([]std.Tx, 0, len(block.Block.Data.Txs))
+	// Gather blocks containing transaction in RPC results
+	var blocks []*client.Block
 
-	for _, encodedTx := range block.Block.Data.Txs {
-		var tx std.Tx
-
-		if unmarshalErr := amino.Unmarshal(encodedTx, &tx); unmarshalErr != nil {
-			return nil, fmt.Errorf(
-				"unable to unmarshal amino tx, %w",
-				err,
-			)
+	for _, result := range results {
+		blockRes, ok := result.(*ctypes.ResultBlock)
+		if !ok {
+			return nil, errors.New("unable to cast request result to TxData")
 		}
 
-		txs = append(txs, tx)
+		// If block contain transaction, gather them
+		if len(blockRes.Block.Data.Txs) > 0 {
+			txs := make([]std.Tx, 0, len(blockRes.Block.Data.Txs))
+
+			// Decode amino transactions
+			for _, encodedTx := range blockRes.Block.Data.Txs {
+				var tx std.Tx
+
+				if unmarshalErr := amino.Unmarshal(encodedTx, &tx); unmarshalErr != nil {
+					return nil, fmt.Errorf(
+						"unable to unmarshal amino tx, %w",
+						err,
+					)
+				}
+
+				txs = append(txs, tx)
+			}
+
+			// Add block including transactions, timestamp and block height to slice
+			blocks = append(blocks, &client.Block{
+				Timestamp: blockRes.Block.Time.UnixMilli(),
+				Height:    uint64(blockRes.Block.Height),
+				Txs:       txs,
+			})
+		}
 	}
 
-	return &client.Block{
-		Timestamp: block.Block.Time.UnixMilli(),
-		Height:    blockNum,
-		Txs:       txs,
-	}, nil
+	return blocks, nil
 }
